6b27a83605f55a783e2e8eb1b60102cbfed01b5c
Thomas Weichselbaumer <kontakt@themecoder.de>
2020-03-18 15:29:13 +0100

    adjusted layout of footer area

--- footer.php
+++ footer.php
@@ -24,8 +24,8 @@
 	<footer id="colophon" class="site-footer">
 
 		<?php
-			get_template_part( 'template-parts/footer/footer', 'widgets' );
 			get_template_part( 'template-parts/footer/footer', 'menus' );
+			get_template_part( 'template-parts/footer/footer', 'widgets' );
 			get_template_part( 'template-parts/footer/footer', 'copyright' );
 		?>
 
