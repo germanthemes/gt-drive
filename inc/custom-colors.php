fc8ab264293c67db0ff640758a3187c2e8e62c7c
Thomas Weichselbaumer <kontakt@themecoder.de>
2020-03-20 15:44:50 +0100

    added light text and border colors if dark background is chosen

--- inc/custom-colors.php
+++ inc/custom-colors.php
@@ -56,6 +56,17 @@ class GT_Drive_Custom_Colors {
 		// Color Variables.
 		$color_variables = '';
 
+		// Set Text Color if dark background color was chosen.
+		if ( self::is_color_dark( get_theme_mod( 'background_color' ) ) ) {
+			$color_variables .= '--text-color: #ffffff;';
+			$color_variables .= '--light-text-color: rgba(255, 255, 255, 0.5);';
+			$color_variables .= '--post-meta-color: rgba(255, 255, 255, 0.5);';
+			$color_variables .= '--light-border-color: rgba(255, 255, 255, 0.1);';
+			$color_variables .= '--medium-border-color: rgba(255, 255, 255, 0.3);';
+			$color_variables .= '--widget-border-color: rgba(255, 255, 255, 0.1);';
+			$color_variables .= '--comments-border-color: rgba(255, 255, 255, 0.1);';
+		}
+
 		// Set Primary Color.
 		if ( $theme_options['primary_color'] !== $default['primary_color'] ) {
 			$color_variables .= '--primary-color: ' . $theme_options['primary_color'] . ';';
